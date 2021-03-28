9fd92d92a226cea32b44f65b270033b674aefe9d
guiferviz <guiferviz@gmail.com>
2021-03-29 00:22:07 +0200

    Adding exception throw to the cpp code.

--- codigo_videos/algoritmos/a000_max/solver.cpp
+++ codigo_videos/algoritmos/a000_max/solver.cpp
@@ -4,17 +4,26 @@ using namespace std;
  
 
 int solve(int n, vector<int> a) {
+    if (a.size() == 0) {
+        throw invalid_argument("empty list of numbers");
+    }
+
     int max = a[0];
     for (int i=1; i<n; i++) {
         if (a[i] > max) {
             max = a[i];
         }
     }
+
     return max;
 }
 
 
 int solve_another_implementation(int n, vector<int> a) {
+    if (a.size() == 0) {
+        throw invalid_argument("empty list of numbers");
+    }
+
     int max = a[--n];
     while (n--) {
         if (a[n] > max) {
