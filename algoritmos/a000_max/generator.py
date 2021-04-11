585e7ce35dcd534bd8c877ca5ec25c3a85fbdbcd
guiferviz <guiferviz@gmail.com>
2021-04-11 02:03:21 +0200

    Improving generator with click

--- algoritmos/a000_max/generator.py
+++ algoritmos/a000_max/generator.py
@@ -1,27 +1,32 @@
 import random
-import sys
+
+import click
 
 
+MIN_a_i = 0
 MAX_a_i = 2147483648
 
 
 def generate_sample(n):
     print(n)
 
-    a = list()
-    for i in range(n):
-        a.append(random.randint(0, MAX_a_i-1))
+    a = [random.randint(MIN_a_i, MAX_a_i-1) for _ in range(n)]
     print(" ".join(map(str, a)))
 
 
-def main():
-    if len(sys.argv) == 3:
-        n = int(sys.argv[1])
-        random.seed(int(sys.argv[2]))
-        t = 1
-    else:
-        t = random.randint(1, 50)
-        n = random.randint(1, 100)
+@click.command()
+@click.option("--test-cases", "-t", type=int,
+              help="Number of test cases to generate in the same test file.")
+@click.option("--n-numbers", "-n", type=int,
+              help="Number of input numbers per test case.")
+@click.option("--seed", "-s", type=int,
+              help="Integer random seed.")
+def main(test_cases, n_numbers, seed):
+    if seed is not None:
+        random.seed(seed)
+
+    t = random.randint(1, 50) if test_cases is None else test_cases
+    n = random.randint(1, 100) if n_numbers is None else n_numbers
 
     print(t)
     for _ in range(t):
