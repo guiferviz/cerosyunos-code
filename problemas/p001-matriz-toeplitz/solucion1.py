b7f1a33bb8d736bcd44431d7211809414c407e5b
guiferviz <guiferviz@gmail.com>
2021-06-06 22:07:01 +0200

    Renaming solutions

--- problemas/p001-matriz-toeplitz/solucion1.py
+++ problemas/p001-matriz-toeplitz/solucion1.py
@@ -1,9 +1,23 @@
-def solve(n, m, matrix):
-    for i in range(n-1):
-        for j in range(m-1):
-            if matrix[i][j] != matrix[i+1][j+1]:
-                return 0
-    return 1
+def es_toeplitz(n_filas, n_columnas, matriz):
+    for i in range(n_columnas):
+        if not es_diagonal_constante(0, i, matriz, n_filas, n_columnas):
+            return False
+    for i in range(1, n_filas):
+        if not es_diagonal_constante(i, 0, matriz, n_filas, n_columnas):
+            return False
+    return True
+
+
+def es_diagonal_constante(f, c, matriz, n_filas, n_columnas):
+    primer_numero = matriz[f][c]
+    f += 1
+    c += 1
+    while f < n_filas and c < n_columnas:
+        if matriz[f][c] != primer_numero:
+            return False
+        f += 1
+        c += 1
+    return True
 
 
 def read_input():
@@ -18,8 +32,8 @@ def read_input():
 
 def main():
     n, m, matrix = read_input()
-    is_toeplitz = solve(n, m, matrix)
-    print(is_toeplitz)
+    is_toeplitz = es_toeplitz(n, m, matrix)
+    print(int(is_toeplitz))
 
 
 if __name__ == "__main__":
